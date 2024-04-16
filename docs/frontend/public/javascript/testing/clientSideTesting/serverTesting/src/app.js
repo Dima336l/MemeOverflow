e406d7e262f54a0c794235d56b90957bbefcaa96
Dumitru Nirca <Nircadmitrii@icloud.com>
2024-04-16 22:20:47 +0100

    Delete docs directory

--- docs/frontend/public/javascript/testing/clientSideTesting/serverTesting/src/app.js
+++
@@ -1,8 +0,0 @@
-import { expect } from "chai";
-import { isValidEmail } from "../../../../validation.mjs";
-
-describe ("isValidEmail", () => {
-    it("should validate the email correctly", () => {
-        expect(isValidEmail("Nircadmitrii@icloud.com")).to.equal(true);
-    })
-})
